410035a0c320ef578fa6dff11148c75277e3288b
Bakhtiyor22 <baxtiyorabduraximov2203@gmail.com>
2025-02-27 18:45:10 +0500

    Commit

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,13 +1,22 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+        return functionMap.containsValue(requiredValue);
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 1; i < sourceList.size(); i++) {
+            map.put(i, 5 * sourceList.get(i) + 2);
+        }
+
+        return map;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,24 @@
 package com.epam.mjc.collections.map;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> map = new HashMap<>();
+        Arrays.stream(sentence.toLowerCase().split("\\W+"))
+                .filter(word -> !word.isEmpty())
+                .forEach(word -> map.merge(word, 1, Integer::sum));
+//        String[] words = sentence.toLowerCase().split("\\W+");
+//
+//
+//        for (String word : words) {
+//            if (!word.isEmpty()) {
+//                map.put(word, map.getOrDefault(word, 0) + 1);
+//            }
+//        }
+        return map;
     }
 }
